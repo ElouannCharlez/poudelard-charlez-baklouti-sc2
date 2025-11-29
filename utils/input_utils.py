d5e1bad4fca527851fa755b1089906f61a736f72
ElouannCharlez <charlezelouann@gmail.com>
2025-11-29 17:51:17 +0100

    ajout de la fonction demander_nombre dans input_utils

---
+++ utils/input_utils.py
@@ -0,0 +1,8 @@
+def demander_nombre(message, min_val=None, max_val=None):
+    val=int(input(message))
+    while (min_val!=None and min_val>val) or (max_val!=None and max_val<val):
+        val = int(input(message))
+    return val
+
+# print(demander_nombre("nb entre 1-9 : ",1,9))
+# entrée(0) : sortie(redemande); entrée(1) : sortie(renvoie 1)
