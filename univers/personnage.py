7547904f522ac73a2ef7a23edf7437581efd5d7f
ElouannCharlez <charlezelouann@gmail.com>
2025-12-10 14:56:18 +0100

    personnage réglé avec maison

--- univers/personnage.py
+++ univers/personnage.py
@@ -1,40 +1,44 @@
 def initialiser_personnage(nom, prenom, attributs):
-    return {"Nom": nom, "Prenom": prenom, "Argent": 100, "Inventaire": [], "Sortilèges": [], "Attributs": attributs}
+    return {"Nom": nom, "Prenom": prenom, "Argent": 100, "Inventaire": [], "Sortilèges": [], "Attributs": attributs, "Maison": "aucune"}
 
-# print(initialiser_personnage("Baklouti","Youssef",{'courage : 8'}))
-# renvoie : {'Nom': 'Baklouti', 'Prenom': 'Youssef', 'Argent': 100, 'Inventaire': [], 'Sortilèges': [], 'Attributs': {'courage : 8'}}
+# if __name__ == '__main__':
+#     print(initialiser_personnage("Baklouti","Youssef",{}))
 
 def afficher_personnage(joueur) :
     print("Profil du personnage : ")
-    print("Nom : ",joueur["Nom"])
-    print("Prenom : ",joueur["Prenom"])
-    print("Argent : ",joueur["Argent"])
+    print("Nom : ", joueur["Nom"])
+    print("Prenom : ", joueur["Prenom"])
+    print("Argent : ", joueur["Argent"])
     print("Inventaire : ")
     for val in joueur["Inventaire"]:
-        print("- ",val)
+        print("- ", val)
     print("Sortilèges : ")
     for val in joueur["Sortilèges"]:
-        print("- ",val)
+        print("- ", val)
     print("Attributs : ")
     for val in joueur["Attributs"]:
         print("- ", val, " : ", joueur["Attributs"][val])
+    print("Maison : ", joueur["Maison"])
     return None
 
-# perso=initialiser_personnage("Baklouti","Youssef",{'courage : 8'})
-# afficher_personnage(perso)
+if __name__ == "__main__":
+    perso=initialiser_personnage("Baklouti","Youssef",{})
+    afficher_personnage(perso)
 
 def modifier_argent(joueur, montant):
     joueur["Argent"] += montant
     return None
 
-# perso=initialiser_personnage("Baklouti","Youssef",{'courage : 8'})
-# modifier_argent(perso,-10)
-# afficher_personnage(perso)
+# if __name__ == "__main__":
+#     perso=initialiser_personnage("Baklouti","Youssef",{})
+#     modifier_argent(perso,-10)
+#     afficher_personnage(perso)
 
 def ajouter_objet(joueur, cle, objet):
     joueur[cle].append(objet)
     return None
 
-# perso=initialiser_personnage("Baklouti","Youssef",{'courage : 8'})
-# ajouter_objet(perso, "Inventaire", "baguette magique")
-# afficher_personnage(perso)
+# if __name__ == "__main__":
+#     perso=initialiser_personnage("Baklouti","Youssef",{})
+#     ajouter_objet(perso, "Inventaire", "baguette magique")
+#     afficher_personnage(perso)
