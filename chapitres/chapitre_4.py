9cf12d31c733dbae5e3c0bb68dd2c3712320cd64
ElouannCharlez <charlezelouann@gmail.com>
2025-12-11 14:51:22 +0100

    début chap4

---
+++ chapitres/chapitre_4.py
@@ -0,0 +1,37 @@
+import random
+
+def creer_equipe(maison, equipe_data, est_joueur=False, joueur=None):
+    pass
+
+def tentative_marque(equipe_attaque, equipe_defense, joueur_est_joueur=False):
+    pass
+
+def apparition_vifdor():
+    return random.randint(1,6)==6
+
+# if __name__ == '__main__':
+#     for i in range(10):
+#         print(apparition_vifdor())
+
+def attraper_vifdor(e1, e2):
+    attrapeur=random.randint(1,2)
+    if attrapeur==1:
+        return e1
+    return e2
+
+if __name__=='__main__':
+    e1=
+    e2=
+    print(attrapeur(e1,e2))
+
+def afficher_score(e1, e2):
+    pass
+
+def afficher_equipe(maison, equipe):
+    pass
+
+def match_quidditch(joueur, maisons):
+    pass
+
+def lancer_chapitre4_quidditch(joueur, maisons):
+    pass
